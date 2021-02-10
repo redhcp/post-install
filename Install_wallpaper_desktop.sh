4c4f3113f4d1d3b444fcee00485a80119769d93e
redhcp <lerner20037@gmail.com>
2021-02-10 13:38:26 -0300

    update

--- Install_wallpaper_desktop.sh
+++ Install_wallpaper_desktop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 
-wget  -P /home/$USER https://github.com/redhcp/testACG/archive/main.zip
+wget  -P /home/$USER/ https://github.com/redhcp/post-install/archive/main.zip
+cd /home/$USER/
 sudo unzip -o ~/main
 
-gsettings set org.gnome.desktop.background picture-uri "file:///home/$USER/testACG-main/edrans-desktop-fhd-dark.png"
+rm -f main.zip*
+
+gsettings set org.gnome.desktop.background picture-uri "file:///home/$USER/post-install-main/edrans-desktop.png"
 
--- install_post.sh
+++ install_post.sh
@@ -5,31 +5,30 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
-echo "***ACTUALIZADO***"
 
 sudo apt install wget
-echo -e "\e[44mFin App System\e[0m"
+echo -e "\e[44m-----------Fin App System\e[0m"
 
 #CHROME
-cd /home/$USER/Downloads
+cd /home/$USER/
 wget https://dl.google.com/linux/linux_signing_key.pub
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
-echo -e "\e[44mFin Chrome\e[0m"
+echo -e "\e[44m-----------Fin Chrome\e[0m"
 
 #SLACK
-cd /home/$USER/Downloads
+cd /home/$USER/
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.12.2-amd64.deb
 sudo dpkg -i slack-desktop-*.deb
-echo -e "\e[44mFin Slack\e[0m"
+echo -e "\e[44m-----------Fin Slack\e[0m"
 
 #ZOOM
-cd /home/$USER/Downloads
+cd /home/$USER/
 wget https://zoom.us/client/latest/zoom_amd64.deb
 sudo dpkg -i zoom*.deb
 sudo apt-get install -f -y
-echo -e "\e[44mFin Zoom\e[0m"
+echo -e "\e[44m-----------Fin Zoom\e[0m"
 
 #
 #
