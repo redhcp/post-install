5f884f2b5044e7bfce9a4331befe9929c1542ad9
redhcp <lerner20037@gmail.com>
2021-03-05 14:52:26 -0300

    updated

--- Install_wallpaper_desktop.sh
+++ Install_wallpaper_desktop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 
-wget  -P /home/$USER/ https://github.com/redhcp/post-install/archive/main.zip
-cd /home/$USER/
-sudo unzip -o ~/main
-
-rm -f main.zip*
-
-gsettings set org.gnome.desktop.background picture-uri "file:///home/$USER/post-install-main/edrans-desktop.png"
-
+sudo wget  -P /home/ https://github.com/redhcp/post-install/archive/main.zip
+cd /home/
+sudo unzip main.zip -d /home/
+cd post-install-main/
+cp edrans-desktop-dark.png /home/
+gsettings set org.gnome.desktop.background picture-uri "file:///home/edrans-desktop.png"
+cd /home/
+sudo rm -f -r post-install*
+sudo rm -f *main.zip
--- install_post.sh
+++ install_post.sh
@@ -5,34 +5,47 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
+echo -e "\e[44m_____Updated_____\e[0m"
 
 sudo apt install wget
-echo -e "\e[44m-----------Fin App System\e[0m"
+echo -e "\e[44m_____End App System_____\e[0m"
 
 #CHROME
-cd /home/$USER/
-wget https://dl.google.com/linux/linux_signing_key.pub
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+cd /home/
+sudo wget https://dl.google.com/linux/linux_signing_key.pub
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
-echo -e "\e[44m-----------Fin Chrome\e[0m"
+sudo rm -f google-chrome*.deb
+sudo rm -f *signing_key.pub
+echo -e "\e[44m_____End Chrome_____\e[0m"
 
 #SLACK
-cd /home/$USER/
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.12.2-amd64.deb
+cd /home/
+sudo wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.12.2-amd64.deb
 sudo dpkg -i slack-desktop-*.deb
-echo -e "\e[44m-----------Fin Slack\e[0m"
+sudo rm -f slack*.deb
+echo -e "\e[44m_____End Slack_____\e[0m"
 
 #ZOOM
-cd /home/$USER/
-wget https://zoom.us/client/latest/zoom_amd64.deb
+cd /home/
+sudo wget https://zoom.us/client/latest/zoom_amd64.deb
 sudo dpkg -i zoom*.deb
-sudo apt-get install -f -y
-echo -e "\e[44m-----------Fin Zoom\e[0m"
+sudo rm -f zoom*.deb
+echo -e "\e[44m_____End Zoom_____\e[0m"
 
-#
-#
 
+#WALLPAPER
+sudo wget  -P /home/ https://github.com/redhcp/post-install/archive/main.zip
+cd /home/
+sudo unzip main.zip -d /home/
+cd post-install-main/
+cp edrans-desktop-dark.png /home/
+gsettings set org.gnome.desktop.background picture-uri "file:///home/edrans-desktop.png"
+cd /home/
+sudo rm -f -r post-install*
+sudo rm -f *main.zip
+#
 
 sudo apt update -y
 exit
