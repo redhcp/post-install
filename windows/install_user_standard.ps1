2672564fd1194133036d8ad30ea06e76cdafd15b
clerner <clerner@edrans.com>
2021-07-08 22:14:02 -0300

    update file

--- windows/install_user_standard.ps1
+++ windows/install_user_standard.ps1
@@ -1,11 +1,18 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -y
+Set-ExecutionPolicy Bypass -Scope Process -Force; `
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-#upgrade choco
-choco upgrade chocolatey 
+choco upgrade chocolatey #upgrade choco
+
 #install apps
 choco install -y --force googlechrome slack zoom adobereader firefox winrar vlc google-drive-file-stream libreoffice-fresh
 
+choco list --local-only  # view list installed apps
 
-#choco install dellcommandupdate  #for DELL laptops
+
+#Others cmd:
+
+#choco install dellcommandupdate  # for DELL laptops
+
+#cup -y all #Upgrade applications with Chocolatey
 
 
