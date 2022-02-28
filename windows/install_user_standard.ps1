21b3bdb462061c52269af6b94b536a7c3c2fdffa
redhcp <43521734+redhcp@users.noreply.github.com>
2022-02-28 11:28:59 -0800

    update download link chocolatey

--- windows/install_user_standard.ps1
+++ windows/install_user_standard.ps1
@@ -1,6 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; `
-  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco upgrade chocolatey #upgrade choco
 
 #install apps
